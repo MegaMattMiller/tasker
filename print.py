aafa9ab5b5c7efb5d74458e59ba1ba703c6a4d96
Matt Miller <matt@mattmiller.dev>
2025-08-04 22:38:43 -0500

    feat: formatted time for local timezone, prettied up output

--- print.py
+++ print.py
@@ -3,16 +3,33 @@
 # iInterface              0 
 # lsusb -vvv -d 0fe6:811e | grep bEndpointAddress | grep OUT
 # bEndpointAddress     0x01  EP 1 OUT
+from datetime import datetime
 from escpos import *
+from dateutil import tz
 import sys,json
 
+from_zone = tz.tzutc()
+to_zone = tz.tzlocal()
+
 def printTask(taskName, taskDesc, createdAt, assignedTo):
+
+    utc = datetime.strptime(createdAt, "%Y-%m-%dT%H:%M:%S.%fZ")
+    utc = utc.replace(tzinfo=from_zone)
+    central = utc.astimezone(to_zone)
+    date = central.date().strftime("%m-%d-%Y")
+    time = central.time().strftime("%I:%M %p")
+
     p = printer.Usb(0x0fe6, 0x811e)
     p.text(f'{assignedTo}\n')
     p.text('\n')
-    p.text(f'{createdAt}\n')
+    p.text('Created\n')
+    p.text(f'{date} at {time}\n')
+    p.text('\n')
+    p.text('--------------------\n')
     p.text('\n')
     p.text(f'{taskName}\n')
+    p.text('\n')
+    p.text('--------------------\n')
     p.text('\n')
     p.text(f'{taskDesc}\n')
     p.text('\n')
